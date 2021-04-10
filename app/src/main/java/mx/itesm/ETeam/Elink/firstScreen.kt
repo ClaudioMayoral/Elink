ed80586c9469aec8f38c4313edc61efc967365af
Alex Leyva <A01747078@itesm.mx>
2021-04-10 01:05:17 -0400

    Esqueleto de las primeras 2 pantallas de registro. First screen y Login screen

---
+++ app/src/main/java/mx/itesm/ETeam/Elink/firstScreen.kt
@@ -0,0 +1,11 @@
+package mx.itesm.ETeam.Elink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class firstScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_screen)
+    }
+}
