cb2622bfa5a64ce5061085ce05d5e1f172edb294
Alex Leyva <A01747078@itesm.mx>
2021-04-10 12:38:43 -0400

    Agrega las pantallas faltantes para el registro

---
+++ app/src/main/java/mx/itesm/ETeam/Elink/SheepTypeScreen.kt
@@ -0,0 +1,11 @@
+package mx.itesm.ETeam.Elink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SheepTypeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sheep_type_screen)
+    }
+}
---
+++ app/src/main/java/mx/itesm/ETeam/Elink/UserTypeScreen.kt
@@ -0,0 +1,11 @@
+package mx.itesm.ETeam.Elink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserTypeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_type_screen)
+    }
+}
