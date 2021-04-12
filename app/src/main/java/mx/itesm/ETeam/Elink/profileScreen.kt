9ab143c816d90b1cf83ff0121e6124758486d8a6
Alex Leyva <A01747078@itesm.mx>
2021-04-12 18:43:19 -0400

    Implemento la pantalla de perfil

---
+++ app/src/main/java/mx/itesm/ETeam/Elink/profileScreen.kt
@@ -0,0 +1,11 @@
+package mx.itesm.ETeam.Elink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profileScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_screen)
+    }
+}
