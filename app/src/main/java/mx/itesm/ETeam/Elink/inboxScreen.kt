ceaffbd9126465981f0300de080e09e29ec4c2b1
Alex Leyva <A01747078@itesm.mx>
2021-04-12 17:41:16 -0400

    Se implementan parcialmente las pantallas de mensajeria

---
+++ app/src/main/java/mx/itesm/ETeam/Elink/inboxScreen.kt
@@ -0,0 +1,11 @@
+package mx.itesm.ETeam.Elink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inboxScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inbox_screen)
+    }
+}
