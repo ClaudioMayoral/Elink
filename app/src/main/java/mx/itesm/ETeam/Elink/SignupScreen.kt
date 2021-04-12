2c4ce13c1b55fd019c63f381c7733335aec662d7
Alex Torices <a01377744@itesm.mx>
2021-04-12 16:00:27 -0500

    Proyecto conectado a firebase

--- app/src/main/java/mx/itesm/ETeam/Elink/SignupScreen.kt
+++ app/src/main/java/mx/itesm/ETeam/Elink/SignupScreen.kt
@@ -2,10 +2,26 @@ package mx.itesm.ETeam.Elink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import mx.itesm.ETeam.Elink.databinding.ActivitySignupScreenBinding
 
 class SignupScreen : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySignupScreenBinding
+    // Administra la información de sign-in
+    private lateinit var mAuth: FirebaseAuth
+
+    //  ActivityForResult
+    private val RC_SIGN_IN: Int = 200
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup_screen)
+
+        binding = ActivitySignupScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        binding.btnRegistrarse2.setOnClickListener{
+        }
     }
 }
