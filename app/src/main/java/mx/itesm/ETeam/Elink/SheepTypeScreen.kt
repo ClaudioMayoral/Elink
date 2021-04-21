f049348987bca9c6902f6c042f6e83564c5bdf87
francisco <faae00@hotmail.com>
2021-04-21 07:59:32 -0500

    Se arreglaron constraints en las pantallas de login, signup, usertype selection. Tambien se eliminaron las flechas de regreso

--- app/src/main/java/mx/itesm/ETeam/Elink/SheepTypeScreen.kt
+++ app/src/main/java/mx/itesm/ETeam/Elink/SheepTypeScreen.kt
@@ -3,17 +3,32 @@ package mx.itesm.ETeam.Elink
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.activity_sheep_type_screen.*
 import mx.itesm.ETeam.Elink.databinding.ActivitySharkTypeScreenBinding
 import mx.itesm.ETeam.Elink.databinding.ActivitySheepTypeScreenBinding
 
 class SheepTypeScreen : AppCompatActivity() {
+
     private lateinit var binding: ActivitySheepTypeScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySheepTypeScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        configurarDropDown()
+
         //configurarBotones()
+    }
+
+    private fun configurarDropDown() {
+        val category = resources.getStringArray(R.array.categorias)
+        val adapter = ArrayAdapter(this, R.layout.dropdown_item, category)
+        binding.autoCategory.setAdapter(adapter)
     }
 /*
     private fun configurarBotones() {
@@ -25,3 +40,4 @@ class SheepTypeScreen : AppCompatActivity() {
         }
     }*/
 }
+
