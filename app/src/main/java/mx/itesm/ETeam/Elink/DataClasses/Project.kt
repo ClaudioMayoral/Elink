c9549c824095917a38a1dc164fe8a5300cfa2a27
Alex Torices <a01377744@itesm.mx>
2021-05-03 20:46:29 -0500

    Preferencias de usuario y proyectos registrados en base de datos, validación de entradas y selección de imagen en la galería

---
+++ app/src/main/java/mx/itesm/ETeam/Elink/DataClasses/Project.kt
@@ -0,0 +1,4 @@
+package mx.itesm.ETeam.Elink
+
+data class Project(val nombreDelProyecto:String="", val descripcionProyecto:String="",
+                   val moneyGoal:Double=0.0, val categoria:String="")
---
+++ app/src/main/java/mx/itesm/ETeam/Elink/DataClasses/SharkPreferences.kt
@@ -0,0 +1,4 @@
+package mx.itesm.ETeam.Elink
+
+data class SharkPreferences(val ambiental:Boolean, val tecnologia:Boolean,val social:Boolean,
+                            val entretenimiento:Boolean,val lifeStyle:Boolean)
